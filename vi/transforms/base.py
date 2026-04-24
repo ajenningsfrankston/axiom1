bc25e336d7d7440e6679886e1445700cd449860b
ajenningsfrankston <ajenningsfrankston@gmai.com>
2026-04-24 10:06:29 +1000

    re-organised

---
+++ vi/transforms/base.py
@@ -0,0 +1,36 @@
+# Copyright 2025 VERSES AI, Inc.
+#
+# Licensed under the VERSES Academic Research License (the “License”);
+# you may not use this file except in compliance with the license.
+#
+# You may obtain a copy of the License at
+#
+#     https://github.com/VersesTech/axiom/blob/main/LICENSE
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Transforms are distributions that are conditioned on latents.  They model p(y|x,\theta)
+where y is the event, x is the input_event, and \theta gives the parameters.  When x and y
+are observed, the transforms expects:
+
+x.shape = sample_shape + batch_shape + input_event_shape
+y.shape = sample_shape + batch_shape + event_shape
+
+where batch_shape refers to the batch shape of the parameters of the transform.  Note that
+technically the batch dimensions of x and y could include (1,)'s such as when you are doing a
+mixture of linear transforms or performing a batch of optimizations in parallel. But no matter what
+the batch_shape of x and y should allow for broadcasting.
+
+Transforms also have forward and backward routines that emit message distributions.
+"""
+
+from axiom.vi import Conjugate
+
+
+class Transform(Conjugate):
+    pass
---
+++ vi/utils/__init__.py
@@ -0,0 +1,45 @@
+# Copyright 2025 VERSES AI, Inc.
+#
+# Licensed under the VERSES Academic Research License (the “License”);
+# you may not use this file except in compliance with the license.
+#
+# You may obtain a copy of the License at
+#
+#     https://github.com/VersesTech/axiom/blob/main/LICENSE
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from .pytree import (
+    ArrayDict,
+    apply_add,
+    apply_scale,
+    params_to_tx,
+    map_and_multiply,
+    zeros_like,
+    map_dict_names,
+    size,
+    tree_copy,
+    sum_pytrees,
+    tree_marginalize,
+    tree_equal,
+    tree_at,
+)
+
+from .math import (
+    mvgammaln,
+    mvdigamma,
+    stable_logsumexp,
+    stable_softmax,
+    assign_unused,
+    bdot,
+    symmetrise,
+    positive_leading_eigenvalues,
+    make_posdef,
+    inv_and_logdet,
+)
+from .input_handling import check_optim_args, get_default_args
+from .maps import *
